0be80d3b6c143611d0d46fe5c1289ec3e2c8aac0
QuocTuanJV <quoctuan.naf@gmail.com>
2020-01-30 14:52:18 +0700

    upload one image success to home

--- src/main/java/com/example/mydemo/services/impl/HomeServiceImpl.java
+++ src/main/java/com/example/mydemo/services/impl/HomeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.mydemo.services.impl;
 
+import com.example.mydemo.model.Home;
 import com.example.mydemo.repository.HomeRepository;
 import com.example.mydemo.services.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,4 +11,14 @@ public class HomeServiceImpl implements HomeService {
 
     @Autowired
     private HomeRepository homeRepository;
+
+    @Override
+    public Home findHomeById(Long id) {
+        return homeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void saveHome(Home home) {
+        homeRepository.save(home);
+    }
 }
