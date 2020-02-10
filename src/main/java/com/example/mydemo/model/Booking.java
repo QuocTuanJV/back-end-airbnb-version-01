318c8186d65f8453f6ef5a4c4dcd9b17902a82bd
QuocTuanJV <quoctuan.naf@gmail.com>
2020-02-10 11:45:51 +0700

    create model Booking entity

---
+++ src/main/java/com/example/mydemo/model/Booking.java
@@ -0,0 +1,71 @@
+package com.example.mydemo.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table
+@Inheritance(strategy =  InheritanceType.SINGLE_TABLE)
+public class Booking {
+    @Id
+    @GenericGenerator(name="itemIdGen" , strategy="increment")
+    @GeneratedValue(generator="itemIdGen")
+    @Column(name = "id_booking")
+    private Long id;
+
+    private Date timeBook;
+
+    private Date timeBookOut;
+
+    @Column(name = "id_user")
+    private Long idUser;
+
+    @Column(name = "id_home")
+    private Long idHome;
+
+    public Booking(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getTimeBook() {
+        return timeBook;
+    }
+
+    public void setTimeBook(Date timeBook) {
+        this.timeBook = timeBook;
+    }
+
+    public Long getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Long idUser) {
+        this.idUser = idUser;
+    }
+
+    public Long getIdHome() {
+        return idHome;
+    }
+
+    public void setIdHome(Long idHome) {
+        this.idHome = idHome;
+    }
+
+    public Date getTimeBookOut() {
+        return timeBookOut;
+    }
+
+    public void setTimeBookOut(Date timeBookOut) {
+        this.timeBookOut = timeBookOut;
+    }
+}
